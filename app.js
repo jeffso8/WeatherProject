d404fb126680a8e4400f5e4cd29a6032d0cea043
Jeff So <jeffso9820@gmail.com>
2020-09-02 22:10:20 -0400

    add semi-colons

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.get("/", function(req, res) {
 
 
 app.post("/weather", function(req, res) {
-  const query = req.body.city
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=728400902af1c70e12b68411802f073a&units=metric"
+  const query = req.body.city;
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=728400902af1c70e12b68411802f073a&units=metric";
 
   https.get(url, function(response) {
     console.log(response.statusCode);
@@ -29,14 +29,14 @@ app.post("/weather", function(req, res) {
       const temp = weatherData.main.temp;
       const description = weatherData.weather[0].description;
       const icon = weatherData.weather[0].icon;
-      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
       res.write("<body style='background-color: #333;'>");
       res.write("<p style='color:#fff; text-align:center; margin-top:10em;'>The condition is currently " + description + "</p>");
       res.write("<h1 style='color:#fff;text-align:center;'>The temperature in " + query + " is " + temp + " degrees Celcius.</h1>");
       res.write("<center><img src=" + imageURL + " style='width:200px;height:200px'></center>");
       res.write("</body>");
-      res.send()
+      res.send();
     })
   })
 });
